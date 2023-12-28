7cf6ed59a006a8a6f7537381b1322ef2631bc03a
Subha <subharamkie@gmail.com>
2023-12-28 15:41:04 +0000

    Fix:Logo image import

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -1,8 +1,9 @@
 import Nav from "./nav";
+import logoImg from '../images/logo.jpg';
 const Header = () =>
     <header className='header '>
     <h1>Fakelandia Justice department</h1>
-    <img src="../../images/logo.jpg" className="header__img" alt="Fakelandia justice logo"/>
+    <img src={logoImg} className="header__img" alt="Fakelandia justice logo"/>
     <Nav/>
     </header>
 
