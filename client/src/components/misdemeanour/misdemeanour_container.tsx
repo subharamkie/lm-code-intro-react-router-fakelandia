4cf1788ece9d0292f1e624017048c6dccb698bdd
Subha <subharamkie@gmail.com>
2024-01-03 12:24:23 +0000

    Feat:Added try,catch for fetch api and show error message accordingly

--- client/src/components/misdemeanour/misdemeanour_container.tsx
+++ client/src/components/misdemeanour/misdemeanour_container.tsx
@@ -3,6 +3,7 @@ import { MisdemeanourResponse } from "../../types/misdemeanours.types";
 import {MisdemeanourList} from './misdemeanours_list';
 import { MisdemeanourFilter } from "./misdemeanourFilter";
 import { JusticeContext } from "../justiceContext";
+import MessageContainer from "../message/message";
 
 export const MisdemeanourContext = createContext<MisdemeanourResponse>([]);
 
@@ -10,16 +11,22 @@ export const MisdemeanourContainer: React.FC = () => {
     const fetchUrl = 'http://localhost:8080/api/misdemeanours/10';
     const [misdemeanours, setMisdemeanours] = useState<MisdemeanourResponse>([]);
     const [filteredMisdemeanours,setFilteredMisdemeanours] = useState<MisdemeanourResponse>([]);
-
+    const [isError,setIsError] = useState(false);
+    const [errorMsg,setErrorMsg] = useState('');
     const contextObj = useContext(JusticeContext);
     useEffect(() => {
     async function getMisdemeanours() {
+      //do try catch here
       if(contextObj.misdemeanourArray.length === 0){
-        const data = await fetch(fetchUrl);
-        const result = await data.json()
-        setMisdemeanours(result.misdemeanours);
-        //set context?
-        contextObj.updateMisdemeanour(result.misdemeanours);
+        try{
+          const data = await fetch(fetchUrl);
+          const result = await data.json()
+          setMisdemeanours(result.misdemeanours);
+          contextObj.updateMisdemeanour(result.misdemeanours);
+        }catch(error){
+          setErrorMsg(error.message);
+          setIsError(true);
+        }
       }else{
         setMisdemeanours(contextObj.misdemeanourArray);
       }
@@ -36,8 +43,9 @@ export const MisdemeanourContainer: React.FC = () => {
   return (
     <MisdemeanourContext.Provider value={misdemeanours}>
       <h2 className="caption__text">Misdemeanours list</h2>
-      <MisdemeanourFilter filterMisdemeanours={filterResults}/>
-      <MisdemeanourList misdemeanours={filteredMisdemeanours} />
+      {isError && <MessageContainer message={errorMsg}/>}
+      {!isError && <MisdemeanourFilter filterMisdemeanours={filterResults}/>}
+      {!isError && <MisdemeanourList misdemeanours={filteredMisdemeanours} />}
     </MisdemeanourContext.Provider>
   )
 }
